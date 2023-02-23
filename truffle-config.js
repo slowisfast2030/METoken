c7981d39775715bd827e265701cd06d321956729
slowisfast2030 <slowisfast2030@163.com>
2023-02-23 18:25:05 +0800

    all is well

--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,10 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
-
+    
+    // 这里不能用助记词
+    // 在使用助记词的情况下，默认使用accounts[0]进行部署，from参数不起作用
+    // 而account[0]没有ETH，导致部署失败
     sepolia: { 
 			network_id: "11155111",
 			provider: () => new HDWalletProvider(privateKey, 'https://sepolia.infura.io/v3/9faf676500e24b3693d74249d8c8412c'),
